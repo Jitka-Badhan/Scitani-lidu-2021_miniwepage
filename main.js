61bff3e1522e2454739e9c86056e59a8b722d6ba
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-03-29 12:10:21 +0200

    main.js conditions simplified, margins in style.css improved

--- main.js
+++ main.js
@@ -5,23 +5,28 @@ const nowDiv = document.querySelector('.dnes');
 const scitaniDiv = document.querySelector('.scitaniLidu');
 const odkazDiv = document.querySelector('.odkaz a');
 
-const scitaniOnlineStart = dayjs('2021-03-27');
 const scitaniOnlineEnd = dayjs('2021-04-09');
 const scitaniPapirStart = dayjs('2021-04-17');
-const scitaniPapirEnd = dayjs('2021-05-11');
+const scitaniEnd = dayjs('2021-05-11');
 
 nowDiv.innerHTML = `<p>Dnes je ${now.format('DD.MM.YYYY')}</p>`;
 
-if (now >= scitaniOnlineStart && now <= scitaniOnlineEnd) {
+if (now <= scitaniOnlineEnd) {
   scitaniDiv.innerHTML =
     scitaniDiv.innerHTML +
-    '<br><p> Můžeš vyplnit dotazník <strong>online</strong>.</p>';
+    '<p> Můžeš vyplnit dotazník <strong>online</strong>.</p>';
   odkazDiv.style.display = 'inline-block';
-} else if (now >= scitaniPapirStart && now <= scitaniPapirEnd) {
+} else if (now < scitaniPapirStart) {
   scitaniDiv.innerHTML =
     scitaniDiv.innerHTML +
-    '<br><p> Můžeš vyplnit dotazník <strong>v papírové podobě</strong>.</p>';
+    '<p>Už není možné vyplnit dotazník online, ale za pár dní můžeš vyplnit dotazník <strong>v papírové podobě</strong>.</p>';
+  odkazDiv.style.display = 'inline-block';
+  odkazDiv.textContent = 'Více informací zde';
+} else if (now <= scitaniPapirEnd) {
+  scitaniDiv.innerHTML =
+    scitaniDiv.innerHTML +
+    '<p> Můžeš vyplnit dotazník <strong>v papírové podobě</strong>.</p>';
 } else {
   scitaniDiv.innerHTML =
-    scitaniDiv.innerHTML + '<br><p> Sčítání lidu už skončilo.</p>';
+    scitaniDiv.innerHTML + '<p> Sčítání lidu už skončilo.</p>';
 }
